59cda6beb51d99e5f1758e1324db1d47480a8271
boboan <360969885@qq.com>
2024-03-15 11:06:36 +0800

    feat: 前端window组件判断 tauri 环境

--- src/components/Layout/Window.tsx
+++ src/components/Layout/Window.tsx
@@ -7,29 +7,33 @@ export default function () {
   const outlet = useOutlet();
   const location = useLocation();
 
-  console.log(outlet);
+  const header = window.__TAURI__ ? (
+    <Box
+      data-tauri-drag-region=""
+      className="tr-window__titlebar"
+      sx={{
+        position: "relative",
+        zIndex: 9999,
+        height: "30px",
+        background: "#f0f0f0",
+        paddingLeft: "80px",
+        lineHeight: "30px",
+        fontSize: "14px",
+        color: "#333",
+        userSelect: "none",
+        cursor: "default",
+      }}
+    >
+      我是标题,你也可以在这里使用html写其他的ui
+    </Box>
+  ) : null;
+
   return (
     <Box className="tr-window">
-      <Box
-        data-tauri-drag-region=""
-        className="tr-window__titlebar"
-        sx={{
-          position: "relative",
-          zIndex: 9999,
-          height: "30px",
-          background: "#f0f0f0",
-          paddingLeft: "80px",
-          lineHeight: "30px",
-          fontSize: "14px",
-          color: "#333",
-          userSelect: "none",
-        }}
-      >
-        我是标题,你也可以在这里使用html写其他的ui
-      </Box>
       {/* 头部 */}
+      {header}
+      {/* 路由窗口 */}
       <Box className="tr-window__main">
-        {/* {outlet} */}
         <AnimatePresence>
           {outlet ? React.cloneElement(outlet, { key: location.key }) : outlet}
         </AnimatePresence>
